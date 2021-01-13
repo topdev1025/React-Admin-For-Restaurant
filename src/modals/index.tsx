60eba216d4a6f85c71c28d5c830f88ae4e7ca7f9
topdev1025 <bridgething1025@gmail.com>
2021-01-13 23:05:59 +0800

    push_original_project

---
+++ src/modals/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class RRR extends React.Component<any,any>{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default RRR;
